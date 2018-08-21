0e60cbd49e5c54887ce7407506129746867f8440
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-08-20 23:22:23 -0700

    defined objects for 3 players

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+// declare the array 'words' to store all words that could be guessed during the game
+var words = [michaelJordan, shawnKemp, shaq]
+
+
+//define object 'michaelJordan' with following properties
+var michaelJordan = {
+    name: "Michael Jordan",
+    team: "Chicago Bulls",
+    jersey: 23,
+    inhall: true
+}
+//define object 'shawnKemp' with following properties
+var shawnKemp = {
+    name: "Shawn Kemp",
+    team: "Seattle SuperSonics",
+    jersey: 40,
+    inhall: false
+}
+//define object 'shaq' with following properties
+var shaq = {
+    name: "Shaquille O'Neal",
+    team: "Los Angeles Lakers",
+    jersey: 32,
+    inhall: true
+}
+
+console.log(words)
+
