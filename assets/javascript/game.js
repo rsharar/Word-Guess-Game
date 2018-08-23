08fba8ce3fc67681f10f16f992dc18c994bb9f0f
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-08-23 09:14:32 -0700

    guesses remaining decreases with incorrect guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,7 +28,7 @@ var guessesLeft = 10;
 var wrongGuesses = [];
 
 // declare variable for userInput from keyboard event
-// var userInput;
+var userInput;
 
 // randomly chosen word from wordbank
 var chosenWord; 
@@ -79,24 +79,29 @@ var underScores = [];
         if (alphabet.indexOf(userInput) >=0) {
             // console.log userInput
             console.log("userInput:", userInput);
-            var userInputPush = document.getElementById('letters-guessed').innerHTML = userInput;
+            // need to push userInput to HTML but store all guesses as an array
+            for (k = 0; k < 1000; k++){
+            var userInputPush = document.getElementById('letters-guessed').innerHTML = userInput[k];
+            }
             // check if userInput matches index of chosenWord
                    // if userInput is in chosenWord replace underscore with letter in correct position
                    // if userInput is in currentWord console.log 'in the word'
                 if (currentWord.indexOf(userInput) > -1){
                         for (j = 0; j < currentWordLength; j++){
-                            if (currentWord[j] === userInput){
-                                underScores[j] === userInput;
-                                console.log(underScores)
+                            if (userInput = ' ___ '){
+                                userInput === underScores[j];
+                                console.log(underScores);
                             }
                         }
                     console.log("in the word")
                 }
                 else{
                     console.log("letter not in word")
-                    guessesLeft--
-                    var guessesLeft = document.getElementById('guesses-remaining').innerHTML.guessesLeft
-                }
+                    // derease guessesLeft by 1
+                    guessesLeft--;
+                    // change guessesLeft in HTML
+                    var guessesLeftPush =document.getElementById('guesses-remaining').innerHTML = guessesLeft;
+                    }                
             }
                     // else decrease guesses remaining by 1 and add userInput to wrongGuesses array
 
